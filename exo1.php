b1cc03a48c1405b3c50751bb34b78fbcac94fe4e
luc-hostein <luc.host44@laposte.net>
2020-03-19 11:53:46 +0100

    work

--- exo1.php
+++ exo1.php
@@ -4,6 +4,8 @@ echo "<h3>Exo 1</h3><br><br><br>";
 class equipeFoot{
     private $nom;
     private $nbTitre;
+    const message = "Nombre d'équipe : ";
+    static protected $nbEquipe=0;
     public function display(){
         echo "L'équipe ".$this->nom." à ".$this->nbTitre." titre(s)";
     }
@@ -16,13 +18,34 @@ class equipeFoot{
         $this->nbTitre = $nbTitre;
     }
 
+    static function displayNbEquipe(){
+        echo self::message;
+        echo self::$nbEquipe;
+    }
+
     function __construct($nom,$nbTitre){
         $this->setNom($nom);
         $this->setNbTitre($nbTitre);
+        self::$nbEquipe++;
+    }
+
+    function __destruct()
+    {
+        echo"<br>destruct";
     }
 }
 
 $equipe1 = new equipeFoot("OM",2);
+$equipe2 = new equipeFoot("PSG",6);
+$equipe3 = new equipeFoot("FC nantes",4);
+
 $equipe1->display();
+echo"<br>";
+$equipe2->display();
+echo"<br>";
+$equipe3->display();
+echo"<br>";
+equipeFoot::displayNbEquipe();
+
 
 ?>
